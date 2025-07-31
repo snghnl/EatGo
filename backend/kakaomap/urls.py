4808d5cbc1e2d1e39e95e9d661e31da7c10b34f3
snghnl <viselacity@gmail.com>
2025-07-31 21:38:24 +0900

    feat: make the feature of recommending based on user category preference
    - Make the model UserPreference
    - Make the API for calculating the basic distance
    - Make the basic algorithm for calculating recommendation
    - Make the API endpoint

--- backend/kakaomap/urls.py
+++ backend/kakaomap/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import get_kakao_map, search_places_from_kakao
+from .views import (
+    get_kakao_map,
+    search_places_from_kakao,
+    calculate_distance,
+    get_route_info_from_kakao,
+    calculate_multi_point_distance,
+)
 
 urlpatterns = [
-    path('map/', get_kakao_map, name='get_kakao_map'),
-    path('search/', search_places_from_kakao, name='search_places_from_kakao'),
-] 
+    path("map/", get_kakao_map, name="get_kakao_map"),
+    path("search/", search_places_from_kakao, name="search_places_from_kakao"),
+    path("distance/", calculate_distance, name="calculate_distance"),
+    path("route/", get_route_info_from_kakao, name="get_route_info"),
+    path(
+        "optimize-route/",
+        calculate_multi_point_distance,
+        name="calculate_multi_point_distance",
+    ),
+]
