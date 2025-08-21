50042b5912aec7a41e3c115ef31bcd305ba5e6bf
snghnl <viselacity@gmail.com>
2025-08-21 21:49:30 +0900

    feat: add feature about Category

--- backend/eatgo/urls.py
+++ backend/eatgo/urls.py
@@ -73,6 +73,7 @@ urlpatterns = [
     # api
     path(settings.API_VERSION + "/kakaomap/", include("kakaomap.urls")),
     path(settings.API_VERSION + "/accounts/", include("accounts.urls")),
+    path(settings.API_VERSION + "/", include("core.urls")),
     path(settings.API_VERSION + "/routes/", include("routes.urls")),
     path(settings.API_VERSION + "/travel_courses/", include("travel_courses.urls")),
     path(settings.API_VERSION + "/tourism/", include("tourism.urls")),
