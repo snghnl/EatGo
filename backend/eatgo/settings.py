32ca8fa797470403ad081b7719865788468e7dac
snghnl <viselacity@gmail.com>
2025-08-24 16:30:53 +0900

    feat: migrate into postgresql

--- backend/eatgo/settings.py
+++ backend/eatgo/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = "eatgo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("DB_NAME", "eatgo_db"),
+        "USER": os.environ.get("DB_USER", "admin"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
